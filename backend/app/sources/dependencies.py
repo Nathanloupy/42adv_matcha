56d4e42b2a23dae7159717469ebcd29c560c8250
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-17 10:42:37 +0100

    feat: using dependencies and starting login endpoint

---
+++ backend/app/sources/dependencies.py
@@ -0,0 +1,13 @@
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+
+from typing import Annotated
+from sqlmodel import Session
+from pwdlib import PasswordHash
+
+from . import database
+
+oauth2_scheme: OAuth2PasswordBearer	= OAuth2PasswordBearer(tokenUrl="token")
+password_hash: PasswordHash			= PasswordHash.recommended()
+token								= Annotated[str, Depends(oauth2_scheme)]
+session								= Annotated[Session, Depends(database.get_database_session)]
