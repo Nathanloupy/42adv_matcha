647f5b3251cb71179c0e2a5ec5ab7bf2d5195c22
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-17 12:10:38 +0100

    feat: atempt to use jwt token login

--- backend/app/sources/routers/auth.py
+++ backend/app/sources/routers/auth.py
@@ -1,14 +1,25 @@
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 from pydantic import BaseModel
+from datetime import datetime, timedelta, timezone
 from sqlalchemy import text
 from sqlmodel import Session
 from pwdlib import PasswordHash
+import jwt
+from jwt.exceptions import InvalidTokenError
 
 from .. import dependencies
 
 router: APIRouter = APIRouter()
+
+# Todo: change secret key
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+TOKEN_EXPIRE_MINUTES = 60
+ALGORITHM = "HS256"
+
+class Token(BaseModel):
+	access_token: str
+	token_type: str
 
 class Signup(BaseModel):
 	username: str
@@ -17,9 +28,11 @@ class Signup(BaseModel):
 	surname: str
 	firstname: str
 
-class Login(BaseModel):
-	username: str
-	password: str
+def generate_token(data: dict) -> str:
+	to_encode: dict = data
+	expire: datetime = datetime.now(timezone.utc) + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+	to_encode.update({"exp": expire})
+	return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 @router.post("/signup/", tags=["auth"])
 async def signup(session: dependencies.session, signup: Signup):
@@ -46,14 +59,15 @@ async def signup(session: dependencies.session, signup: Signup):
 		raise HTTPException(status_code=400)
 
 @router.post("/login/", tags=["auth"])
-async def login(session: dependencies.session, login: Login):
+async def login(session: dependencies.session, login: dependencies.oauth2_request_form):
 	try:
 		query = text("SELECT * FROM users WHERE username = :username")
 		result = session.execute(query, {"username": login.username})
 		user = result.fetchone()
 		if not dependencies.password_hash.verify(login.password, user.password):
 			raise ValueError()
-		return {"access_token": user.username, "token_type": "bearer"}
+		token = generate_token({"sub": user.username})
+		return Token(access_token=token, token_type="bearer")
 	except ValueError:
 		raise HTTPException(status_code=401)
 	except Exception:
