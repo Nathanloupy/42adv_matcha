8ba66e6fbe153e46ab208caf19674a892f51d174
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-17 15:06:16 +0100

    feat: jwt auth

--- backend/app/sources/dependencies.py
+++ backend/app/sources/dependencies.py
@@ -1,13 +1,17 @@
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
+import os
 from typing import Annotated
 from sqlmodel import Session
 from pwdlib import PasswordHash
 
 from . import database
 
-oauth2_scheme: OAuth2PasswordBearer	= OAuth2PasswordBearer(tokenUrl="token")
+jwt_secret: str						= os.getenv("JWT_SECRET", "changeme")
+jwt_algorithm: str					= os.getenv("JWT_ALGORITHM", "HS256")
+jwt_token_expire: int				= int(os.getenv("JWT_TOKEN_EXPIRE", "10"))
+oauth2_scheme: OAuth2PasswordBearer	= OAuth2PasswordBearer(tokenUrl="login")
 password_hash: PasswordHash			= PasswordHash.recommended()
 token								= Annotated[str, Depends(oauth2_scheme)]
 session								= Annotated[Session, Depends(database.get_database_session)]
--- backend/app/sources/routers/users.py
+++ backend/app/sources/routers/users.py
@@ -1,11 +1,32 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+from sqlalchemy import text
+from sqlmodel import Session
+import jwt
+
+from .. import dependencies
 
 router: APIRouter = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-token_dep = Annotated[str, Depends(oauth2_scheme)]
 
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+class TokenData(BaseModel):
+    username: str | None = None
+
+@router.get("/users/me", tags=["users"])
+async def me(token: dependencies.token, session: dependencies.session):
+	try:
+		payload = jwt.decode(token, dependencies.jwt_secret, algorithms=[dependencies.jwt_algorithm])
+		username = payload.get("sub")
+		if username is None:
+			raise ValueError
+		token_data = TokenData(username=username)
+		query = text("SELECT * FROM users WHERE username = :username")
+		result = session.execute(query, {"username": token_data.username})
+		user = result.fetchone()
+		return {"username": user.username, "password": user.password}
+	except ValueError:
+		raise HTTPException(status_code=401)
+	except Exception:
+		raise HTTPException(status_code=400)
+	
