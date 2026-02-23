6029b5b741ee1be2555878bb2d29360bfc1a1515
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-23 11:12:12 +0100

    feat: basic browsing

--- backend/app/sources/routers/browsing.py
+++ backend/app/sources/routers/browsing.py
@@ -13,17 +13,23 @@ from .. import dependencies
 router: APIRouter = APIRouter()
 
 @router.get("/browse", tags=["borwsing"])
-async def browse(session: dependencies.session, request: Request):
-	user_query: TextClause = text("SELECT id FROM users WHERE username = :username")
+async def browse(session: dependencies.session, request: Request, offset: int = 0):
+	user_query: TextClause = text("SELECT * FROM users WHERE username = :username")
 	query: TextClause = text("""
-		SELECT *, COUNT(users_tags.id) as tag_count FROM users
+		SELECT username, firstname, surname, age, gender, biography, gps, fame, last_connection, COUNT(users_tags.id) as tag_count FROM users
 		LEFT JOIN users_tags ON users.id = users_tags.user_id
-			AND users_tags IN (
+			AND users_tags.tag IN (
 				SELECT tag FROM users_tags WHERE user_id = :current_user_id
 			)
+		LEFT JOIN users_likes ON users.id = users_likes.other_id
+			AND users_likes.user_id != :current_user_id
 		WHERE username != :username
+		AND users_likes.id IS NULL
+		AND completed == 1
+		AND (gender == :gender OR gender == :gender2)
 		GROUP BY users.id
 		ORDER BY tag_count DESC, fame DESC
+		LIMIT 10 OFFSET :offset
 	""")
 
 	token = request.cookies.get("access_token")
@@ -35,16 +41,26 @@ async def browse(session: dependencies.session, request: Request):
 		if username is None:
 			raise HTTPException(status_code=400)
 		result = session.execute(user_query, {"username": username})
-		return {"ok": "ok"}
 		user = result.fetchone()
 		if user is None:
 			raise HTTPException(status_code=404)
-		result = session.execute(query, {"current_user_id": user.id, "username": user.username})
+		if user.completed == 0:
+			raise HTTPException(status_code=400, detail="user profile is not completed")
+		oposite_gender: int = 0 if user.gender == 1 else 1
+		genders: list = [oposite_gender, oposite_gender]
+		if user.sexual_preference == 2:
+			genders[1] = user.gender
+		result = session.execute(query, {
+			"current_user_id": user.id,
+			"username": user.username,
+			"gender": genders[0],
+			"gender2": genders[1],
+			"offset": offset
+		})
 		users = result.fetchall()
 		if users is None:
 			raise HTTPException(status_code=404)
-		users = dict(user._mapping)
-		return users
+		return [dict(user._mapping) for user in users]
 	except HTTPException:
 		raise
 	except Exception as exception:
